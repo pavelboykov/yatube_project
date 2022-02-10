94fca4ec2cd57a83e289ab4a6bd2de59ec0d3d84
Павел Бойков <boykov.pavel@gmail.com>
2022-02-10 09:47:30 +0300

    Реализован проект спринта 3

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,24 @@
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Главная страница социальной сети блогеров')
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Последние обновления на сайте'
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Пост: {slug}')
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = f'Записи сообщества {str(group)}'
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
